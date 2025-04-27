9d2df57829992f7f188f29981b60330a687a466a
Vimal Ramnarain <vimalramnarain@gmail.com>
2025-04-27 03:09:29 -0400

    Update TodoDao functions

--- app/src/main/java/com/vimal/todoapp/db/TodoDao.kt
+++ app/src/main/java/com/vimal/todoapp/db/TodoDao.kt
@@ -1,8 +1,20 @@
 package com.vimal.todoapp.db
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.vimal.todoapp.Todo
 
 @Dao
 interface TodoDao {
+    @Query("SELECT * FROM Todo")
+    fun getAllTodo(): LiveData<List<Todo>>
 
+    @Insert
+    fun addTodo(todo: Todo)
+
+    @Query("DELETE FROM Todo WHERE id = :id")
+    fun deleteTodo(id: Int)
 }
+
